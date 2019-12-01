0a63b54b97b2f3509c65caa4836fce4100bed5f1
gkgoat <gkgoat6700@gmail.com>
2019-12-01 13:48:14 -0500

    Update app.ts

--- src/app-framework/app.ts
+++ src/app-framework/app.ts
@@ -1,5 +1,7 @@
 export default ((window as any).olApps = class App extends Object{
     private _data: any;
+    public name: string;
+    public closeListeners: Array<Function>;
     static get registry(){return (this as any)._registry || ((this as any)._registry = new Map())}
 static register(name: string,data: any): Function{
     let old = this.registry.get(name);
@@ -11,8 +13,9 @@ renderWindow(h: any,prev: any): any{
 return h(h.window,{onClose: this.onClose.bind(this)},[prev])
 
 }
-onClose(): void{
-
-
+onClose(): Promise<any>{
+let mp = Promise.all(this.closeListeners.map(l => l(this))).then(a => ({listenerResults: a}));
+let rfr = Promise.resolve(this.name && fetch('https://universe.ol.com/' + this.name + '/ping').then(r => r.text()).catch(err => undefined));
+return Promise.all([mp,rfr]).then(r => ({listenerResults: r[0],fetchResult: r[1]}))
 }
 })
