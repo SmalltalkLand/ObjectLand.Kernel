c34cc5c2f3cca9b9b912a6255a30fa5ee809c978
gkgoat <gkgoat6700@gmail.com>
2019-12-17 15:16:01 -0500

    fixes

--- src/app-manager.ts
+++ src/app-manager.ts
@@ -1,7 +1,12 @@
 export default class AppManager{
+    allApps: any[];
+    getCurrent: () => any;
+    setCurrent: (v: any) => void;
 constructor(theAllApps: Array<any>,opts: {getCurrent: () => any,setCurrent: (v: any) => void}){
+this.allApps = theAllApps;
+this.getCurrent = opts.getCurrent;
+this.setCurrent = opts.setCurrent;
 
-    
 }
 
 }
